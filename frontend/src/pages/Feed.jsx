36407ea46c75293f085f9e723a31530e040e0eea
Athena Chang <athenachang17@gmail.com>
2025-05-01 21:21:24 -0400

    Feed.jsx: Added structure and Sort By dropdown menu, need to implement filter function

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,12 +1,37 @@
 import Post from "../components/Post";
 
 /**
- * Need the length of all the posts' in the database
- * Each post needs event_id as a prop
- * From there, we can retrieve all of the data from each event_id to render
+ * Feed should display different Posts based on the area when zoomed in or out
+ * Each post will receive event as a prop
+ * Feed will show up on the side bar on top of the Map
+ * Feed should have filtered Posts by city (most recent), borough (most recent), most recent Posts, and probably based on status
  * @returns
  */
 
-export default function Feed() {
-  return <Post eventId={} />;
+export default function Feed({ events }) {
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  return (
+    <>
+      <select className="sort">
+        <option value="city">City</option>
+        <option value="borough">Borough</option>
+        <option value="recent">Most Recent</option>
+        <option value="status">Status</option>
+      </select>
+
+      <div className="feed">
+        {events.map((event) => (
+          <Post
+            key={event.event_id}
+            event={event}
+            onSelect={setSelectedEvent}
+          />
+        ))}
+      </div>
+      {selectedEvent && (
+        <Modal event={selectedEvent} onClose={() => setSelectedEvent(null)} />
+      )}
+    </>
+  );
 }
