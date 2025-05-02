68f43f9d0e1c3b4e8ec7588b1f2546ecccc557e7
Athena Chang <athenachang17@gmail.com>
2025-05-01 22:49:31 -0400

    Profile.jsx: created structure

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,10 +1,31 @@
 import SiteHeadingAndNav from "../components/SiteHeadingAndNav";
 
-export default function Profile ({}) => {
-    return (
-        <>
-            <SiteHeadingAndNav />
-            <h1>@{event.username}</h1>
-        </>
-    )
+export default function Profile({ user, events }) {
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  return (
+    <>
+      <SiteHeadingAndNav />
+
+      <div className="profile-header">
+        <h1>@{user.username}</h1>
+        <h3>
+          {user.first_name} {user.last_name}
+        </h3>
+        <h3>{user.email}</h3>
+      </div>
+
+      <div className="profile">
+        {events.filter(
+          (event) =>
+            event.user_id === user.id && (
+              <Post key={event.id} event={event} onSelect={setSelectedEvent} />
+            )
+        )}
+        {selectedEvent && (
+          <Modal event={selectedEvent} onClose={() => setSelectedEvent(null)} />
+        )}
+      </div>
+    </>
+  );
 }
