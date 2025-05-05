704de8d97a84ef8a05c26a3efeadd119200ab1a5
Athena Chang <athenachang17@gmail.com>
2025-05-04 23:58:45 -0400

    Button.jsx: add more props and revised the button format

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,12 +1,17 @@
 import { NavLink } from "react-router-dom";
 
-export default function Button({ name, onClick, to }) {
-  if (to) {
-    return (
-      <NavLink to={to}>
-        <button onClick={onClick}>{name}</button>
-      </NavLink>
-    );
-  }
-  return <button onClick={onClick}>{name}</button>;
+export default function Button({
+  name,
+  onClick,
+  to,
+  type = "button",
+  ...props
+}) {
+  const btn = () => {
+    <button onClick={onClick} type="type" {...props}>
+      {name}
+    </button>;
+  };
+
+  return to ? <NavLink to={to}>{btn}</NavLink> : btn;
 }
