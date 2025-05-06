999e672986cf4d9a53634b547d63ca0f3cadb1e9
Athena Chang <athenachang17@gmail.com>
2025-05-05 22:06:50 -0400

    ProfilePage.jsx: Added ToDo comments. Started added the logic for currentUser to go into Edit mode to edit their username. Created the structure of how this would work and look like.

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,8 +1,34 @@
 import Button from "../components/Button";
 import SiteHeadingAndNav from "../components/SiteHeadingAndNav";
+import UpdateUsernameForm from "../components/UpdateUsernameForm";
+
+/**
+ * User should be able to edit their username here
+ * User should be able to view their own posts and the posts they've upvoted
+ * User should be able to sort by favorited or their posts
+ * This sorting will be by most recent posts for either both their posts and upvoted
+ * OR
+ * Sorting favorited by the timestamp they were favorited
+ * Need a function somewhere where it fetch all the posts from the user and all the favorited posts if it's current user
+ * @returns
+ */
 
 export default function ProfilePage({ user, events }) {
+  // Need to clarify the events from the user
+  // const userEvents = ;
+  // Need to clarify the events the current user upvoted
+  // const upvotedEvents = ;
+
+  const isNew = !event?.id;
+  const isEditableByUser =
+    isNew || (currentUser && currentUser.id === event.user_id);
+  const [isEdit, setIsEdit] = useState(isNew);
+
   const [selectedEvent, setSelectedEvent] = useState(null);
+
+  const toggleEditMode = () => {
+    setIsEdit(!isEdit);
+  };
 
   return (
     <>
@@ -10,6 +36,8 @@ export default function ProfilePage({ user, events }) {
 
       <div className="profile-header">
         <h1>@{user.username}</h1>
+        <Button name="Edit" onClick={toggleEditMode} />
+        {isEdit && <UpdateUsernameForm />}
         <h3>
           {user.first_name} {user.last_name}
         </h3>
