3136d9ef8e4e2f3b190a1aeb548bd73587e00a4f
Athena Chang <athenachang17@gmail.com>
2025-05-07 13:44:10 -0400

    reorganized migrations and seed data

--- server/db/migrations/20230423034706_create_users_table.js
+++ server/db/migrations/20230423034706_create_users_table.js
@@ -2,63 +2,85 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema
 
-    .createTable('users', function(table) {
-      table.increments('id').primary();
-      table.string('email').notNullable().unique();
-      table.string('first_name').notNullable();
-      table.string('last_name').notNullable();
-      table.string('password_hash').notNullable();
-      table.string('username').notNullable().unique();
+    .createTable("users", function (table) {
+      table.increments("id").primary();
+      table.string("first_name").notNullable();
+      table.string("last_name").notNullable();
+      table.string("email").notNullable().unique();
+      table.string("username").notNullable().unique();
+      table.string("password_hash").notNullable();
     })
 
-
-    .createTable('event', function(table) {
-      table.increments('id').primary();
-      table.string('title').notNullable();
-      table.string('description').notNullable();
-      table.timestamp('date_created').defaultTo(knex.fn.now());
-      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE');
-      table.boolean('is_issue').notNullable();
-      table.string('email').nullable();
-      table.string('phone').nullable();
-      table.string('status').notNullable();
-      table.string('images'); // cloudinary setup as stretch
-      table.decimal('lat_location', 10, 7).nullable();
-      table.decimal('long_location', 10, 7).nullable();
-      table.string('address').nullable();
-      table.string('borough').notNullable();
-      table.string('zipcode').notNullable();
+    .createTable("event", function (table) {
+      table.increments("id").primary();
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE");
+      table.timestamp("date_created").defaultTo(knex.fn.now());
+      table.boolean("is_issue").notNullable();
+      table.string("images"); // cloudinary setup as stretch
+      table.string("title").notNullable();
+      table.decimal("lat_location", 10, 7).nullable();
+      table.decimal("long_location", 10, 7).nullable();
+      table.string("address").nullable();
+      table.string("borough").notNullable();
+      table.string("zipcode").notNullable();
+      table.string("email").nullable();
+      table.string("phone").nullable();
+      table.string("status").notNullable();
+      table.string("description").notNullable();
     })
 
-
-    .createTable('comments', function(table) {
-      table.increments('id').primary();
-      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE');
-      table.string('contents').notNullable();
-      table.timestamp('time').defaultTo(knex.fn.now());
-      table.integer('event_id').unsigned().references('id').inTable('event').onDelete('CASCADE');
+    .createTable("comments", function (table) {
+      table.increments("id").primary();
+      table
+        .integer("event_id")
+        .unsigned()
+        .references("id")
+        .inTable("event")
+        .onDelete("CASCADE");
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE");
+      table.string("contents").notNullable();
+      table.timestamp("time").defaultTo(knex.fn.now());
     })
 
-    .createTable('upvote', function(table) {
-      table.increments('id').primary();
-      table.integer('event_id').unsigned().references('id').inTable('event').onDelete('CASCADE');
-      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE');
-      table.timestamp('favorited_time').defaultTo(knex.fn.now());
+    .createTable("upvote", function (table) {
+      table.increments("id").primary();
+      table
+        .integer("event_id")
+        .unsigned()
+        .references("id")
+        .inTable("event")
+        .onDelete("CASCADE");
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE");
+      table.timestamp("favorited_time").defaultTo(knex.fn.now());
     });
 };
-
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema
-    .dropTableIfExists('upvote')
-    .dropTableIfExists('comments')
-    .dropTableIfExists('event')
-    .dropTableIfExists('users')
+    .dropTableIfExists("upvote")
+    .dropTableIfExists("comments")
+    .dropTableIfExists("event")
+    .dropTableIfExists("users");
 };
