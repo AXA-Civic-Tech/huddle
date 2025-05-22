9c27089cecb6826788eca5da644df752f16b4f92
Autumn Lydon <autumnlydon@gmail.com>
2025-05-22 09:56:58 -0400

    add debugging for deployment

--- server/db/knex.js
+++ server/db/knex.js
@@ -1,3 +1,23 @@
+console.log('==> [DEBUG] NODE_ENV:', process.env.NODE_ENV);
+console.log('==> [DEBUG] PG_CONNECTION_STRING:', process.env.PG_CONNECTION_STRING ? '[SET]' : '[NOT SET]');
+console.log('==> [DEBUG] PG_HOST:', process.env.PG_HOST);
+console.log('==> [DEBUG] PG_USER:', process.env.PG_USER);
+console.log('==> [DEBUG] PG_DB:', process.env.PG_DB);
+
 const env = process.env.NODE_ENV || 'development';
-const config = require('../knexfile')[env];
+const configFile = require('../knexfile');
+console.log('==> [DEBUG] knexfile keys:', Object.keys(configFile));
+console.log('==> [DEBUG] knexfile[env]:', configFile[env]);
+
+const config = configFile[env];
+if (!config) {
+  throw new Error(`[DEBUG] Knex config for env "${env}" is undefined!`);
+}
+if (!config.client) {
+  throw new Error(`[DEBUG] Knex config for env "${env}" is missing "client" property!`);
+}
+if (!config.connection) {
+  throw new Error(`[DEBUG] Knex config for env "${env}" is missing "connection" property!`);
+}
+
 module.exports = require('knex')(config);
